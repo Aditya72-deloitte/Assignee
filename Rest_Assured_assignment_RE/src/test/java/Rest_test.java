cfbe54b3def8929d9a0ada54692ebc17706e2e25
Aditya72 <adityakumar72@deloitte.com>
2022-04-12 14:12:05 +0530

    Validated Unique ID and JSON Schema

--- Rest_Assured_assignment_RE/src/test/java/Rest_test.java
+++ Rest_Assured_assignment_RE/src/test/java/Rest_test.java
@@ -6,7 +6,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AnyOf.anyOf;
@@ -36,11 +39,6 @@ public class Rest_test {
         JSONArray arr = obj.getJSONArray("data");
         for(int i = 0 ; i < arr.length();i++){
             assertThat(arr.getJSONObject(i).get("gender"), anyOf(is("male"), is("female")));
-//            System.out.println(arr.getJSONObject(i).get("gender"));
-//            String gender = (String) arr.getJSONObject(i).get("gender");
-//            assertThat(gender,hasItems("male","female"));
-
-            //assertThat(response.path((String) arr.getJSONObject(i).get("gender")), is(equalTo("male")));
         }
     }
     @Test
@@ -54,12 +52,40 @@ public class Rest_test {
         int flag = 0;
         for (int i = 0; i < arr.length(); i++) {
             String email = (String) arr.getJSONObject(i).get("email");
-            System.out.println(email);
-            if(email.endsWith(".biz")){
-                flag = flag+1;
+            if (email.endsWith(".biz")) {
+                flag = flag + 1;
             }
-          if(flag<2) {
-              Assert.fail();
-          }
+            if (flag < 2) {
+                Assert.fail(".biz extension should be more than 1");
+            }
+        }
+    }
+    @Test
+    public void Unique_id_value(){
+        Response response = given().
+                when().
+                get("https://gorest.co.in/public/v1/users").
+                then().extract().response();
+        JSONObject obj = new JSONObject(response.asString());
+        JSONArray arr = obj.getJSONArray("data");
+        int[] id = new int[arr.length()];
+        for(int i = 0 ; i < arr.length(); i++) {
+           id[i] = (int)arr.getJSONObject(i).get("id");
+        }
+        Arrays.sort(id);
+        for (int i = 0 ; i < (id.length)-1 ; i++) {
+            if(id[i] == id[i+1]){
+                Assert.fail();
+            }
+        }
+    }
+    @Test
+    public void ValidateJsonSchema(){
+        given().
+                baseUri("https://gorest.co.in/public/v1").
+                when().
+                get("/users").
+                then().
+                body(matchesJsonSchemaInClasspath("JSON_schema.json"));
     }
 }
